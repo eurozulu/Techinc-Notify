be98f08581f2aec62de2f4c4116f7a7cd075a688
Rob <eurospoofer@gmail.com>
2012-06-23 17:14:02 +0200

    Moved build level from Level 10 (2.33) to Level 7 (2.1) to allow older android users.

--- src/org/spoofer/techinc/PreferenceSettings.java
+++ src/org/spoofer/techinc/PreferenceSettings.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -21,6 +22,8 @@ public class PreferenceSettings extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Log.v(LOG_TAG, "creating preferences activity");
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
